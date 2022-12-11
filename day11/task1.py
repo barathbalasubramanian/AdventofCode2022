5abe714fd2c29cf15b15bece85d5ceaa4e311d25
barath kumar <barathkumar.b2411@gmail.com>
2022-12-11 22:40:33 +0530

    day11

--- day11/task1.py
+++ day11/task1.py
@@ -21,7 +21,6 @@ for i in data_move :
             available_mon.append(each[7:8])
 
         elif 'Starting items' in each :
-            
             item.append(each[18:])
 
         elif 'Operation' in each :
@@ -48,7 +47,6 @@ for i in operation :
     operator.append(i[-2])
     operand.append(i[-1])
 
-
 print("AVAILABLE MONKEY :" , available_mon)
 print("Starting items   :" ,items)
 print("Operator  :" , operator , 'Operand :' , operand)
@@ -56,9 +54,12 @@ print("Test   :" ,Test)
 print('True :', true)
 print('False : ', false)
 
+monks = [0, 0, 0, 0, 0, 0, 0, 0]
+
 def main() :
-    for i in range(4) :
-        for index,j in enumerate( items[i] ):
+    for i in range(available_mon) :
+        to_remove = []
+        for j in  items[i] :
             mult = 0
 
             if operand[i] == 'old' : 
@@ -79,8 +80,19 @@ def main() :
                 # print(int(false[i]))
                 items[int(false[i])].append(mult)
             
-    # print("Starting items   :" , items)
+            to_remove.append([j,i])
+            monks[i] += 1
 
-for i in range(19) :
+        for i in to_remove :
+            items[i[1]].remove(i[0])
+                
+for i in range(20) :
     main()
-    print('finish',i)
+
+for i in items:
+    print(i)
+
+print(monks)
+monks.sort()
+monks.reverse()
+print(monks[0] * monks[1])
